539f28ea5118e6050ce744f33fca5685e47c1039
Darkrider123 <cosmin.ariton@gmail.com>
2023-10-05 13:50:45 +0300

    first commit

---
+++ app/src/main/java/com/example/wolclient/Config.java
@@ -0,0 +1,8 @@
+package com.example.wolclient;
+
+public class Config {
+
+    public static String host = ConfigThatShouldntAppearOnGit.host;
+    public static Integer port = ConfigThatShouldntAppearOnGit.port;
+
+}
